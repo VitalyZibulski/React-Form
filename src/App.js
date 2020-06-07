3b74d2833970d9b93e5b2d7a0212303332010454
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-06-07 09:59:12 +0300

    initiial project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let statuses = ["DRAFT", "PUBLIC"]
+
+export default function CreatePostForm() {
+
+  return <div className="p-3">
+    <h1 className="h3 mb-3">Create Post</h1>
+    <form autoComplete="off"
+          style={{maxWidth: "800px"}}>
+
+      <div className="form-group">
+        <label>Title</label> ({"*"})<br/>
+        <input name="title"
+               className="form-control"
+               type="text"
+               value={""} />
+      </div>
+      <div className="form-group">
+        <label>Body</label> ({"*"})<br/>
+        <textarea name="body"
+                  rows={5}
+                  className="form-control"
+                  value={""}
+                  />
+      </div>
+      <div className="form-group">
+        <label>Status</label><br/>
+        <select name="status" value={""} className="form-control">
+          {statuses.map(status => <option key={status}>{status}</option>)}
+        </select>
+      </div>
+      <div>
+        <button className="btn btn-primary">
+          Create
+        </button>
+      </div>
+    </form>
+  </div>
 }
 
-export default App;
